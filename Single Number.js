028ef6894131477afbbc425c86341b0615b3cccf
libk <mrlibk@foxmail.com>
2017-03-25 23:47:57 +0800

    添加任务

---
+++ Single Number.js
@@ -0,0 +1,17 @@
+/**
+ Given an array of integers, every element appears twice except for one. Find that single one.
+
+ Note:
+ Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    var result = nums[0];
+    for(var i=1; i<nums.length; i++) {
+        result ^= nums[i];
+    }
+    return result;
+};
